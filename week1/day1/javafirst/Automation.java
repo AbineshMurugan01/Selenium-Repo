aee03c58dcc714b63af1396c29c1b80fd8f491fc
AbineshMurugan01 <165746225+AbineshMurugan01@users.noreply.github.com>
2024-04-02 08:57:07 +0530

    Add files via upload
    Week1

---
+++ week1/day1/javafirst/Automation.java
@@ -0,0 +1,10 @@
+package javafirst;
+
+public class Automation {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("Welcome to TestLead");
+	}
+
+}
---
+++ week1/day1/javafirst/Browser.java
@@ -0,0 +1,23 @@
+package javafirst;
+
+public class Browser {
+
+	public static void main(String arg[]) {
+		
+		float browserversion = 110.00f;
+		
+		String browserName = "Microsoft Edge";
+		
+		boolean isvisible = true;
+		
+		int releaseYear = 1998;
+		
+		char browserlogo = 'Z';
+		
+		System.out.println("This is browser version:" +browserversion);
+		
+		System.out.print(browserName+"\n" +isvisible+ "\n" +releaseYear+ "\n" +browserlogo+ "\n");
+	}
+	
+	
+}
