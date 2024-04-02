0481d7079cc3580a372f0e854678377adeced74f
AbineshMurugan01 <165746225+AbineshMurugan01@users.noreply.github.com>
2024-04-02 08:55:26 +0530

    Add files via upload
    Week1

---
+++ week1/day1/DataTypes.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		int age = 22;
+		long mobile = 8939857741L;
+		char intial = 'M';
+		boolean pass = true;
+		String name = "Abinesh M";
+		
+		System.out.println("My age: " +age);
+		System.out.println("My mobile number :" +mobile);
+		System.out.println("My intial is :" +intial);
+		System.out.println("My Result :" +pass);
+		System.out.println("My name :" +name);
+		
+	}
+
+}
---
+++ week1/day1/Demo.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		 ChromeDriver driver = new ChromeDriver();
+		 
+		 driver.get("http://leaftaps.com/opentaps/control/main");
+	}
+	
+
+}
